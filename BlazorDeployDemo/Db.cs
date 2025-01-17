ad2b3cd110860d99f24764994227fd6272b943da
vik37 <vik.zafirovski@gmail.com>
2025-01-17 21:36:42 +0100

    New Products Navbar - List of Shoe Products

---
+++ BlazorDeployDemo/Db.cs
@@ -0,0 +1,20 @@
+﻿using BlazorDeployDemo.Models;
+
+namespace BlazorDeployDemo
+{
+	public static class Db
+	{
+		public static List<Product> Products = new List<Product>();
+
+		public static void Inital()
+		{
+			if(Products.Count == 0)
+			{
+				Products.Add(new Product(name: "Nike Sneakers", price: 12, count: 2));
+				Products.Add(new Product(name: "Planika Boots", price: 27, count: 7));
+				Products.Add(new Product(name: "Parachute Classic Slippers", price: 8, count: 0));
+				Products.Add(new Product(name: "Converse Seakers", price: 6.25, count: 12));
+			}
+		}
+	}
+}
